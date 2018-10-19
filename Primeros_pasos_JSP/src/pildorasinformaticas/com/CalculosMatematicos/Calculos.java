3895573b8fedb9596ec761cbc55f4e6d91d9c5b0
eucagra <34265809+eucagra@users.noreply.github.com>
2018-10-19 18:12:58 +0200

    Añadidos comentarios

--- Primeros_pasos_JSP/src/pildorasinformaticas/com/CalculosMatematicos/Calculos.java
+++ Primeros_pasos_JSP/src/pildorasinformaticas/com/CalculosMatematicos/Calculos.java
@@ -2,9 +2,10 @@ package pildorasinformaticas.com.CalculosMatematicos;
 
 public class Calculos {
 
-	//
+	//Variable estatica resultado
 	private static int resultado;
 	
+	//Métodos estáticos
 	public static int metodoSuma(int num1, int num2){
 		
 		resultado = num1+num2;
